94a03293ccfc5986cdb223856e64e6d8f9b06b45
Tosin Akinyele <tosinakinyele@Tosins-MacBook-Pro.local>
2021-01-22 21:19:16 +0100

    handle error display

--- controllers/reValidateApi/validate.js
+++ controllers/reValidateApi/validate.js
@@ -1,20 +1,15 @@
 const ruleRunnerService = require('../../services/reValidateApi/ruleRunner');
 const displayError = require('../../helpers/displayError');
 
-function reValidateApi(req, res) {
+function reValidateApi(req, res, next) {
   try {
     const result = ruleRunnerService(req.body);
-    // const { field } = result.validation;
-    // if (!result.validation.error) {
-    //   return new SuccessResponse(`field ${field} successfully validated.`, result, 200).send(res);
-    // }
-    // return new ValidationFailResponse(`field ${field} failed validation.`, result, 400).send(res);
-    // if (result instanceof Error) {
-    //   return displayError(result, res, result.httpStatusCode);
-    // }
+    if (result instanceof Error) {
+      return displayError(result, res, result.httpStatusCode);
+    }
     return res.status(200).json(result);
   } catch (error) {
-    return displayError(error, res, error.httpStatusCode);
+    return next(error);
   }
 }
 
